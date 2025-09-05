8887e3c50adcd1d205090c28c7135b3ab9fe0a94
tjgrace7 <121319122+tjgrace7@users.noreply.github.com>
2025-09-05 07:02:21 -0500

    Updated index.html and vite.config.js to increase mobile page load speeds

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: true,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+          if (id.includes('src/components')) {
+            return 'components'
+          }
+        }
+      }
+    }
+  }
 })
